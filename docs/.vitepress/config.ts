ce8e6c9fdcfdbabb13726c4f23df68c4b45cffc5
elohsnts <eloisadossantos3005@gmail.com>
2026-05-18 14:21:25 -0300

    Commit1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
         // text: 'Guide',
         items: [
           { text: 'Example', link: '/example' },
+          { text: 'Desenvolvimento', link: '/Desenvolvimento'},
           // ...
         ],
       },
